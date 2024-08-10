15d336cb3b8e6a643522b70a896a882de5f505c1
0xLPircy <taveesha.agarwal@gmail.com>
2024-08-10 21:17:05 +0530

    any url process

--- entities/LlamaJsonOnClick.lua
+++ entities/LlamaJsonOnClick.lua
@@ -17,10 +17,12 @@
 local json = require('json')
 
 -- Configure this to the process ID of the world you want to send chat messages to
-CHAT_TARGET = 'YOUR_WORLD_PID'
+CHAT_TARGET = 'YOU_WORLD_ID'
 
 _0RBIT = "BaMK1dfayo75s3q1ow6AO64UDpD9SEFbeE8xYrY2fyQ"
 _0RBT_TOKEN_PROCESS = "BUhZLMwQ6yZHguLtJYA5lLUa9LQzLXMXRfaq9FVcPJc"
+
+change = false
 
 function Validate0rbtQuantity(quantity)
   return quantity == 1000000000000
@@ -35,6 +37,8 @@ Handlers.add('Schema', Handlers.utils.hasMatchingTag('Action', 'Schema'),
 function(msg)
 	print('Schema')
 	local sender = msg.From
+  change = false
+  print("in schema".. tostring(change))
 	Send({
 		Target = msg.From,
 		Tags = {
@@ -79,7 +83,13 @@ Handlers.add('SendReq',
   Handlers.utils.hasMatchingTag('Action', 'SendReq'), 
     function(msg)
       print("SAYING Send")
+      print("in send req".. tostring(change)) 
       local url = msg.Tags["X-Response"] 
+      local prefix = "https://dummyjson.com/recipes/"
+      if(string.sub(url, 1, #prefix) ~= prefix) then 
+        change = true
+        print("in if cond".. tostring(change))
+      end
       print(msg.Tags.Quantity)
       Send({
               Target = _0RBT_TOKEN_PROCESS,
@@ -97,6 +107,7 @@ Handlers.add(
     "Receive-Data",
     Handlers.utils.hasMatchingTag("Action", "Receive-Response"),
     function(msg)
+        print("in recieve".. tostring(change))
         local res = json.decode(msg.Data)
         -- USE constrained string to display some of the data raw, long strings are skipped and not displayed
         local constrainedString = string.sub(json.encode(res), 1, 150)
@@ -106,6 +117,7 @@ Handlers.add(
         local difficultyLevel =""
         local nameOf =""
         local cookTime =""
+        if change == false then
         -- Traverses through decoded json to find key value pairs and assign to local variable for better display
         for k, v in pairs(res) do
           if k == "name" then
@@ -127,5 +139,17 @@ Handlers.add(
           Data = "Recipe is " .. nameOf .. " with a difficulty level of " .. difficultyLevel .. " and cook time of " .. cookTime .." minutes. Have Fun!",
         })
         print("Recipe is " .. nameOf .. " with a difficulty level of " .. difficultyLevel .. " and cook time of " .. cookTime .." minutes. Have Fun!")
+      -- PROCESS A GENERAL URL WITHOUT FORMATTING
+      else
+        Send({
+          Target = CHAT_TARGET,
+          Tags = {
+            Action = 'ChatMessage',
+            ['Author-Name'] = '0rbit Llama',
+          },
+          Data = "Data is: " .. constrainedString .. "..." ,
+        })
+        print(constrainedString)
+      end
     end
 )
